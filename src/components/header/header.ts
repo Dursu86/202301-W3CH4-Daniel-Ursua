6f313eb82776b35882837d3484e9b46ee34b5a0a
Daniel <dursuaf@gmail.com>
2023-02-02 20:14:23 +0100

    Add header and update index

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,1 +1,24 @@
+/* eslint-disable no-new */
+/* eslint-disable no-unused-vars */
 
+import { Component } from '../component/component';
+
+export class Header extends Component {
+  constructor(public selector: string, public title: string = 'Learning DOM') {
+    super();
+    this.template = this.createTemplate();
+    this.render('afterbegin');
+  }
+
+  private createTemplate() {
+    return `
+      <header class="main-header">
+      <h1 class="main-title">My Series</h1>
+    </header>
+    `;
+  }
+
+  render(place: globalThis.InsertPosition) {
+    super.render(place);
+  }
+}
